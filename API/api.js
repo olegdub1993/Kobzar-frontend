6a1ac20839b37fbe7d8dbb32e6b1e785cce30c6c
olegdub1993 <harshaankan923@gmail.com>
2022-12-19 18:59:58 +0200

    diff

--- API/api.js
+++ API/api.js
@@ -14,7 +14,7 @@ $api.interceptors.request.use((config) => {
 $api.interceptors.response.use((config) => {
   return config;
 },async (error) => {
-  const originalRequest = error.config;
+  const originalRequest = error?.config;
   if (error?.response?.status == 401 && error.config && !error.config._isRetry) {
       originalRequest._isRetry = true;
       try {
