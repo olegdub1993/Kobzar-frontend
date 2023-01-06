3915a69624c570668d8bb47107f25b5627b45a84
olegdub1993 <harshaankan923@gmail.com>
2023-01-06 15:11:45 +0200

    added document

--- components/UserItem.tsx
+++ components/UserItem.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 import { Card, Grid } from '@mui/material';
 import { useRouter } from 'next/router';
 import { IUser } from './../types/user';
-
+import albomPicture from './../public/kobza.jpg'
+import Image from 'next/image';
 interface UserItemProps {
     user: IUser
 }
@@ -15,7 +16,9 @@ const UserItem: React.FC<UserItemProps> = ({ user }) => {
             className=' flex flex-col items-center m-[10px] p-[10px]  bg-black pr-6 pl-6 hover:!scale-105 hover:!shadow-lg !transition-all rounded  !duration-500  w-[230px] min-w-[230px] shadow-sm cursor-pointer'
             onClick={() => router.push("/users/" + user._id)}
         >
-            <div className='m-auto w-[150px] h-[150px] mb-8 mt-2 '> <img className='w-[100%] h-[100%] object-cover rounded-full' src={process.env.NEXT_PUBLIC_BASIC_URL + user.picture} /></div>
+            <div className='m-auto w-[150px] h-[150px] mb-8 mt-2 '>
+               <Image alt="User picture" className='w-[100%] h-[100%] object-cover rounded-full' width={150} height={150} src={user.picture ? process.env.NEXT_PUBLIC_BASIC_URL + user.picture : albomPicture} />
+             </div>
             <Grid container className="max-w-full text-red" direction={"column"}>
                 <div className="font-bold truncate text-center max-w-full mb-4">{user.username}</div>
             </Grid>
